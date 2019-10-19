fedbc01c0bb77808bb464bf7eef13a7c4b239439
egorsego <egorsego@gmail.com>
2019-10-19 16:11:03 +0300

    build on commit trigger test

--- src/test/java/com/otus/javaqa/AllureReportExampleTest.java
+++ src/test/java/com/otus/javaqa/AllureReportExampleTest.java
@@ -13,7 +13,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
+//
 public class AllureReportExampleTest {
     private TestStep step;
 
